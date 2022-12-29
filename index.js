5acad1fe87c24b2e9f72075d2a2cc71648bc1fba
delower68 <hdelower68@gmail.com>
2022-12-29 12:39:04 +0800

    data save and show on UI

--- index.js
+++ index.js
@@ -1,15 +1,83 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tkreg8z.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("LinkedIn2").collection("users");
+    const postCollection = client.db("LinkedIn2").collection("posts");
+
+    //post an user data in db
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put("/users", async (req, res) => {
+        const user = req.body;
+
+        const options = { upsert: true };
+  
+        // const updatedDoc = {
+        //   $set: {
+        //     type: "admin",
+        //   },
+        // };
+        const result = await usersCollection.updateOne(
+          user,
+        //   updatedDoc,
+          options
+        );
+        res.send(result);
+      });
+    // a single post sent to the database
+    app.post("/post", async (req, res) => {
+        const post = req.body;
+        const result = await postCollection.insertOne(post);
+        res.send(result);
+    });
+    
+    //Get all post from mongodb
+    app.get("/posts", async (req, res) => {
+      const query = {};
+      const posts = await postCollection.find(query).toArray();
+      res.send(posts);
+    });
+
+    // single post details  
+    app.get('/posts/:id', async(req, res)=>{
+        const id =req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await postCollection.findOne(query);
+        res.send(result);
+    });
+    
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("LinkedIN 2.0 server is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`LinkedIN 2.0 running on ${port}`);
-  });
-  
+  res.send("LinkedIN 2.0 server is running");
+});
+
+app.listen(port, () => {
+  console.log(`LinkedIN 2.0 running on ${port}`);
+});
