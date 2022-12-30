ec94a4ab16c08c235309c3545768ff60e3341b0a
delower68 <hdelower68@gmail.com>
2022-12-30 12:40:58 +0800

    find email for edit profile

--- index.js
+++ index.js
@@ -30,60 +30,53 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/users", async (req, res) => {
-        const user = req.body;
 
-        const options = { upsert: true };
-  
-        // const updatedDoc = {
-        //   $set: {
-        //     type: "admin",
-        //   },
-        // };
-        const result = await usersCollection.updateOne(
-          user,
-        //   updatedDoc,
-          options
-        );
-        res.send(result);
-      });
+    // get all booking data from mongodb
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      const query = { email: email };
+      const userEmail = await usersCollection.find(query).toArray();
+      res.send(userEmail);
+    });
+
+
     // a single post sent to the database
     app.post("/post", async (req, res) => {
-        const post = req.body;
-        const result = await postCollection.insertOne(post);
-        res.send(result);
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
     });
-    
+
     //Get all post from mongodb
     app.get("/posts", async (req, res) => {
       const query = {};
-      const posts = await postCollection.find(query).toArray();
+      const posts = await await postCollection.find(query).toArray();
       res.send(posts);
     });
 
-    // single post details  
-    app.get('/posts/:id', async(req, res)=>{
-        const id =req.params.id;
-        const query = {_id: ObjectId(id)};
-        const result = await postCollection.findOne(query);
-        res.send(result);
+    // single post details
+    app.get("/posts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await postCollection.findOne(query);
+      res.send(result);
     });
 
-    app.put('/posts/:id' , async(req, res)=>{
-        const id =req.params.id;
-        const filter = {_id: ObjectId(id)};
-        const user = req.body;
-        console.log(user)
-        const option = {upsert: true};
-        const updatedDoc = {
-            $set: {
-                comment: user.comment
-            }
-        }
-        const result = await postCollection.updateOne(filter, updatedDoc, option);
-        res.send(result);
-    })
-    
+    app.patch("/posts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      console.log(user);
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          comment: user.comment,
+        },
+      };
+      const result = await postCollection.updateOne(filter, updatedDoc, option);
+      res.send(result);
+    });
   } finally {
   }
 }
